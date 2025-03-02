d7ba9b6cdf0536dd1e08fdee2adc853e73156e0c
Josh Turner <joshgt400@gmail.com>
2025-03-02 23:03:37 +0000

    added more functionality and removed JunctionDetails

--- frontend/directions.py
+++ frontend/directions.py
@@ -1,55 +1,49 @@
 from enum import IntEnum, Enum
 import sys
-import sys
 from pathlib import Path
 sys.path.append((Path(__file__).parent.parent / 'backend').resolve().__str__())
-from flowrates import FlowRates
 from lane import Dir
-# Parameters and list of FlowRates [coming from north, east, south, west]
 
+
+class Turn(Enum):
+    AHEAD = 0
+    RIGHT = 1
+    BACK = 2
+    LEFT = 3
+    
+    def __neg__(self):
+        return Turn((self.value + 2) % 4)
+    
 class CardinalDirection(Enum):
+    # All methods which return a list will return the list in clockwise order
     NORTH = 0
     EAST = 1
     SOUTH = 2
     WEST = 3
 
     def to_Dir(self):
-        if self.value == Direction.NORTH.value:
-            return Dir.NORTH
-        elif self.value == Direction.EAST.value:
-            return Dir.EAST
-        elif self.value == Direction.SOUTH.value:
-            return Dir.SOUTH
-        else:
-            return Dir.WEST
+        return [Dir.NORTH, Dir.EAST, Dir.SOUTH, Dir.WEST][self.index]
     
-    def __add__(self, other):
-        if not isinstance(other, int):
-            raise ValueError("Second argument of + must be of type: int")
-        return CardinalDirection((self.value + other) % 4)
+    def __add__(self, other: Turn):
+        # Turn as if heading in cardinal direction
+        if not isinstance(other, Turn):
+            raise ValueError("Second argument of + must be an instance of Direction")
+        return CardinalDirection((self.value + other.value) % 4)
     
-    def __sub__(self, other):
-        if not isinstance(other, int):
-            raise ValueError("Second argument of - must be of type: int")
-        return CardinalDirection((self.value - other) % 4)
+    def __sub__(self, other: Turn):
+        # Turn as if coming from cardinal direction
+        if not isinstance(other, Turn):
+            raise ValueError("Second argument of - must be an instance of Direction")
+        return CardinalDirection((self.value - other.value) % 4)
     
-    def get_opposite(self):
-        return self + 2
-
-    def get_left(self):
-        return self + 1
+    @classmethod
+    def all_except_clockwise(cls, direction):
+        # List of cardinal directions clockwise from exluded
+        return [direction + Turn.RIGHT for i in range(3)]
     
-    def get_right(self):
-        return self - 1
-    
-    def get_other_directions(self):
-        return [self + i for i in range(1,4)]
-    
-    def index_from(self, other):
-        return (self - other) % 4  
-    
-    def get_index_from_direction(self, base):
-        return (self-base-1) % 4      
+    def all_except_anticlockwise(cls, direction):
+        # List of cardinal directions clockwise from exluded
+        return [direction + Turn.LEFT for i in range(3)]
 
     def simple_string(self):
         return ['north','east','south','west'][self.index]
@@ -58,17 +52,3 @@ class CardinalDirection(Enum):
     def index(self):
         return self.value
         
-
-class JunctionDetails():
-    arms = []
-
-    def __iter__(self):
-        return iter(self.arms)
-    
-    def __getitem__(self, index):
-        return self.arms[index]
-    
-    def get_FlowRate_list(self):
-        return self.arms
-
-print(Direction.NORTH)
