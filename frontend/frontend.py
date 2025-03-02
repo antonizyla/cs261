81345f62b599ba3d30d22eebc56fdc730d1b9fbd
anibitri <anibitri7@gmail.com>
2025-03-02 00:10:52 +0000

    frontend updates

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget, QLab
 from inputAndParameterPage import InputAndParameterWidget
 from resultsPage import ResultsWidget
 from visualisation import ImageViewer
+import os
 
 class HomeWindow(QDialog):
     def __init__(self):
@@ -12,7 +13,15 @@ class HomeWindow(QDialog):
         self.setGeometry(100, 100, 400, 400)
 
         layout = QVBoxLayout()
-        self.label = QLabel("Welcome to the Simulation Application!")
+        self.label = QLabel("Welcome to the Traffic Junction Simulator App!\n"
+                    "\n"
+                    "University of Warwick\n"
+                    "\n"
+                    "Department of Computer Science 2025\n"
+                    "\n"
+                    "Authors: Ani Bitri, Krister Hughes, Thomas Phuong, Eshan Sharif, Josh Turner, Antoni Zyla\n"
+                    "\n"
+                    "Click the button below to go to the Input Parameters page.")
         self.button = QPushButton("Go to Input Parameters")
         self.button.clicked.connect(self.go_to_main_app)
 
@@ -42,16 +51,22 @@ class MainApplication(QMainWindow):
 
     def add_tabs(self):
         # Tab 1: Input and Parameters
-        input_tab = InputAndParameterWidget()
-        self.tab_widget.addTab(input_tab, "Input Parameters")
+        self.input_tab = InputAndParameterWidget()
+        self.tab_widget.addTab(self.input_tab, "Input Parameters")
 
         # Tab 2: Simulation 
-        simulation_tab = ImageViewer()  
-        self.tab_widget.addTab(simulation_tab, "Simulation")
+        self.simulation_tab = ImageViewer()  
+        self.tab_widget.addTab(self.simulation_tab, "Simulation")
 
         # Tab 3: Simulation Results
-        results_tab = ResultsWidget()  # or ResultsPage, depending on your class
-        self.tab_widget.addTab(results_tab, "Simulation Results")
+        self.results_tab = ResultsWidget()  # or ResultsPage, depending on your class
+        self.tab_widget.addTab(self.results_tab, "Simulation Results")
+
+        # Connect the "Start Simulation" button to the method to switch to the Simulation tab
+        self.input_tab.submit_button.clicked.connect(self.go_to_simulation_tab)
+
+    def go_to_simulation_tab(self):
+        self.tab_widget.setCurrentWidget(self.simulation_tab)
 
     def closeEvent(self, event):
         print("Application closed.")
