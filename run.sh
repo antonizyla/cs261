7faacf9d4d639d88630f3e05f19abfafc3065d19
Josh Turner <Josh.Turner.1@warwick.ac.uk>
2025-03-09 14:55:46 +0000

    modified to use python3 instead of python command

--- run.sh
+++ run.sh
@@ -2,9 +2,11 @@
 
 if [ ! -d "venv" ]; then
   echo -e "No venv found, creating one\n"
-  python -m venv venv
+  python3 -m venv venv
+
   echo -e "Activating venv\n"
   source venv/bin/activate
+
   echo -e "Installing requirements\n"
   pip install -r frontend/requirements.txt
   pip install -r backend/requirements.txt
@@ -12,7 +14,9 @@ else
   echo -e "Activating venv\n"
   source venv/bin/activate
 fi
+
 echo -e "Starting application\n"
-python frontend/frontend.py
+python3 frontend/frontend.py
+
 echo -e "Deactivating venv\n"
 deactivate
